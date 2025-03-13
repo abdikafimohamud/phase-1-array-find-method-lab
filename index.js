3b2cc57f1bf763e7e3eaf072e3bbe8019ced57dc
abdikafimohamud <185328738+abdikafimohamud@users.noreply.github.com>
2025-03-13 07:00:38 -0700

    find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+  // Example usage:
+  const broncosRecord = [
+    { year: "2014", result: "L" },
+    { year: "2015", result: "W" },
+    { year: "2016", result: "L" }
+  ];
+  
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+  }
+  
+  console.log(superbowlWin(broncosRecord));
